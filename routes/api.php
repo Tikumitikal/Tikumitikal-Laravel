55e02121e6126eedf2e27a8ee4a8e0e956a930d4
NTsaqif <69290981+NTsaqif@users.noreply.github.com>
2023-06-11 05:18:19 +0700

    update routes api for mobile

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\ApiAuthController;
+use App\Http\Controllers\ApiKategoriController;
+use App\Http\Controllers\ApiMejaController;
+use App\Http\Controllers\ApiProductController;
+use App\Http\Controllers\ApiReservasiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +18,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('login', [ApiAuthController::class, 'login']);
+Route::post('register', [ApiAuthController::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('product', [ApiProductController::class, 'all']);
+Route::get('table', [ApiMejaController::class, 'all']);
+Route::get('kategori', [ApiKategoriController::class, 'all']);
+Route::get('product-top', [ApiProductController::class, 'topSoldProducts']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [ApiAuthController::class, 'logout']);
+    Route::get('me', [ApiAuthController::class, 'userlogin']);
+    Route::post('reservasi-add', [ApiReservasiController::class, 'add']);
+    Route::post('cancel', [ApiReservasiController::class, 'cancel']);
+    Route::post('reservasi-status', [ApiReservasiController::class, 'status']);
 });
